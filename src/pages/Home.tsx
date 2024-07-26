37e9d150bb362396922043c091c657edd1333e11
pablobeckg <beckgarreaud@gmail.com>
2024-07-26 14:59:47 +0200

    second commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,11 @@ const Home = () => {
     return (
         <main>
             <Link to="/all-beers">
-                <img className="all-beers home-button" src="public/images/Group 4.jpg" alt="" />
+                <img className="all-beers home-button" src="images/Group 4.jpg" alt="" />
             </Link>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, a, eleifend vitae varius venenatis. </p>
             <Link to="/random-beer">
-                <img className="random-beer home-button" src="public/images/Group 5.jpg" alt="" />
+                <img className="random-beer home-button" src="images/Group 5.jpg" alt="" />
             </Link>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, a, eleifend vitae varius venenatis. </p>
         </main>
